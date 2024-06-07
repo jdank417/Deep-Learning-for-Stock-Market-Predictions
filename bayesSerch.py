a6b57c1ec4aeabbfb331277d44016c50e2b86f33
jdank417 <dankj@wit.edu>
2024-06-07 11:32:05 -0400

    Update Bayesian code

--- bayesSerch.py
+++ bayesSerch.py
@@ -1,5 +1,5 @@
-#READ ME: This one never finishes
-
+#Update: 06/07/24, Code compliles but the Baysian optimization seems
+#to be ineffective in improving predictions
 
 import yfinance as yf
 import pandas as pd
@@ -12,26 +12,29 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from skopt import BayesSearchCV
 from skopt.space import Real, Integer
-from skopt.utils import use_named_args
 from ta.momentum import RSIIndicator
-from ta.trend import MACD
+from ta.trend import MACD, SMAIndicator
 from ta.volatility import BollingerBands
 from scikeras.wrappers import KerasRegressor
 import os
 
+
 def create_model(lstm_units=50, dropout_rate=0.2, optimizer='adam'):
     model = Sequential()
-    model.add(LSTM(lstm_units, return_sequences=True, input_shape=(60, 10)))
+    model.add(LSTM(lstm_units, return_sequences=True,
+                   input_shape=(60, 11)))  # Adjusted input shape to match number of features
     model.add(Dropout(dropout_rate))
     model.add(LSTM(lstm_units, return_sequences=True))
     model.add(Dropout(dropout_rate))
     model.add(LSTM(lstm_units))
     model.add(Dense(50))
-    model.add(Dense(10))  # Output shape: (num_features)
+    model.add(Dense(11))  # Output shape: (num_features)
     model.compile(optimizer=optimizer, loss='mean_squared_error')
     return model
 
-def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-01-01", test_ratio=0.2, benchmark_ticker='VOO'):
+
+def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-01-01", test_ratio=0.2,
+                          benchmark_ticker='VOO'):
     # Step 1: Retrieve Stock Data
     data = yf.download(stock_ticker, start=start_date, end=end_date)
     benchmark_data = yf.download(benchmark_ticker, start=start_date, end=end_date)
@@ -53,9 +56,8 @@ def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-
     bollinger = BollingerBands(data['Close'])
     data['BB_High'] = bollinger.bollinger_hband().fillna(0)
     data['BB_Low'] = bollinger.bollinger_lband().fillna(0)
-
-    # Calculate Moving Average
-    data['50_MA'] = data['Close'].rolling(window=50).mean().fillna(0)
+    data['50_MA'] = SMAIndicator(data['Close'], window=50).sma_indicator().fillna(0)
+    data['100_MA'] = SMAIndicator(data['Close'], window=100).sma_indicator().fillna(0)
 
     # Add benchmark closing prices as a new feature
     data['Benchmark_Close'] = benchmark_data['Close']
@@ -82,7 +84,8 @@ def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-
     print(f"Sharpe Ratio of {stock_ticker}: {sharpe_ratio}")
 
     # Step 4: Prepare Data for LSTM
-    features = ['Close', 'Volume', 'RSI', 'MACD', 'MACD_Signal', 'BB_High', 'BB_Low', 'Rate of Return', '50_MA', 'Benchmark_Close']
+    features = ['Close', 'Volume', 'RSI', 'MACD', 'MACD_Signal', 'BB_High', 'BB_Low', 'Rate of Return', '50_MA',
+                '100_MA', 'Benchmark_Close']
     stock_data = data[features].values
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_data = scaler.fit_transform(stock_data)
@@ -116,7 +119,8 @@ def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-
         print(f"\t- Best score: {best_score}")
         print(f"\t- Best parameters: {best_params}")
 
-    model = KerasRegressor(model=create_model, lstm_units=50, dropout_rate=0.29206627614880376, optimizer='adam', epochs=10, batch_size=32, verbose=0)
+    model = KerasRegressor(model=create_model, lstm_units=50, dropout_rate=0.29206627614880376, optimizer='adam',
+                           epochs=10, batch_size=32, verbose=0)
     bayes_search = BayesSearchCV(estimator=model, search_spaces=search_space, n_iter=10, cv=3, verbose=1, n_jobs=-1)
     bayes_search.fit(x_train, y_train, callback=print_status)
 
@@ -124,7 +128,8 @@ def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-
     print(f"Best Parameters: {best_params}")
 
     # Step 7: Train the final model with best parameters
-    final_model = create_model(lstm_units=best_params['lstm_units'], dropout_rate=best_params['dropout_rate'], optimizer=best_params['optimizer'])
+    final_model = create_model(lstm_units=best_params['lstm_units'], dropout_rate=best_params['dropout_rate'],
+                               optimizer=best_params['optimizer'])
 
     early_stopping = EarlyStopping(monitor='val_loss', patience=5, mode='min', restore_best_weights=True)
     model_dir = f"{stock_ticker}_model"
@@ -133,7 +138,8 @@ def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-
     checkpoint_path = os.path.join(model_dir, "best_model.keras")
     model_checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 
-    final_model.fit(x_train, y_train, batch_size=32, epochs=30, validation_split=0.2, callbacks=[early_stopping, model_checkpoint])
+    final_model.fit(x_train, y_train, batch_size=32, epochs=30, validation_split=0.2,
+                    callbacks=[early_stopping, model_checkpoint])
 
     # Save the final model architecture
     model_json = final_model.to_json()
@@ -148,7 +154,8 @@ def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-
     prediction_start_index = len(data) - len(predictions)
     prediction_dates = data.index[prediction_start_index:]
 
-    valid = pd.DataFrame(index=prediction_dates, data=predictions, columns=[f'Predictions_{feature}' for feature in features])
+    valid = pd.DataFrame(index=prediction_dates, data=predictions,
+                         columns=[f'Predictions_{feature}' for feature in features])
 
     plt.figure(figsize=(16, 28))
 
@@ -171,8 +178,6 @@ def stock_market_analysis(stock_ticker, start_date="2010-01-01", end_date="2024-
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.legend()
-
-
 
     plt.tight_layout(pad=3.0)  # Adjust padding
     plt.subplots_adjust(hspace=0.5)  # Adjust space between plots
